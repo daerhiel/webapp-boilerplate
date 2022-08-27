0f5a1b07c80713e18618da1179c8195bc8d8c930
daerhiel <485120+daerhiel@users.noreply.github.com>
2022-08-27 18:43:13 +0200

    feature(RouteStrategy): Add unit test placeholders for route reuse strategy

--- src/ConceptBed/concept-bed-ui/src/app/extensions/routing-strategy.service.spec.ts
+++ src/ConceptBed/concept-bed-ui/src/app/extensions/routing-strategy.service.spec.ts
@@ -1,16 +1,39 @@
+import { Component } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { RouteReuseStrategy, Routes } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Persistent } from '@app/modules/services/models/persistent';
 
 import { RoutingStrategyService } from './routing-strategy.service';
+@Persistent()
+@Component({})
+class PersistentComponent {
+}
+
+const routes: Routes = [
+  { path: 'persistent', component: PersistentComponent }
+];
 
 describe('RoutingStrategyService', () => {
   let service: RoutingStrategyService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule.withRoutes(routes)
+      ],
+      providers: [
+        { provide: RouteReuseStrategy, useClass: RoutingStrategyService },
+      ]
+    });
     service = TestBed.inject(RoutingStrategyService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should add persist route', () => {
+
   });
 });
