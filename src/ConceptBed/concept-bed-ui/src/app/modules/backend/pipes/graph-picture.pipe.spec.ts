ebeb73dc242f8cc8dc8a98c87e4b496ce5e7aff9
daerhiel <485120+daerhiel@users.noreply.github.com>
2022-09-08 20:41:38 +0200

    feature(Graph): Improve graph picture pipe caching

--- src/ConceptBed/concept-bed-ui/src/app/modules/backend/pipes/graph-picture.pipe.spec.ts
+++ src/ConceptBed/concept-bed-ui/src/app/modules/backend/pipes/graph-picture.pipe.spec.ts
@@ -1,26 +1,50 @@
-import { TestBed } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
+import { inject, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { DomSanitizer } from '@angular/platform-browser';
+import { lastValueFrom } from 'rxjs';
 
 import { GraphClientService } from '../graph-client.service';
 import { GraphPicturePipe } from './graph-picture.pipe';
 
 describe('GraphPicturePipe', () => {
-  let graph: GraphClientService;
-  let sanitizer: DomSanitizer;
+  let controller: HttpTestingController;
+  const environment = 'login.windows.net';
+  const tenantId = '00000000-1234-5678-90ab-abcdef012345';
+  const localAccountId = 'abcdef01-1234-5678-90ab-abcdef012345';
+  const username = 'user.name@microsoft.com';
+  const account = {
+    homeAccountId: "00000000-0000-0000-c41c-d9a99aaa6fe4.9188040d-6c67-4c5b-b112-36a304b66dad",
+    environment, tenantId, username, localAccountId, name: "User Name"
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-        HttpClientModule,
-      ]
-    });
-    graph = TestBed.inject(GraphClientService);
-    sanitizer = TestBed.inject(DomSanitizer);
+        HttpClientTestingModule,
+      ],
+      providers: []
+    }).compileComponents();
+    controller = TestBed.inject(HttpTestingController);
   });
 
-  it('create an instance', () => {
+  afterEach(() => {
+    controller.verify();
+  });
+
+  it('create an instance', inject([GraphClientService, DomSanitizer], async (graph: GraphClientService, sanitizer: DomSanitizer) => {
     const pipe = new GraphPicturePipe(graph, sanitizer);
     expect(pipe).toBeTruthy();
-  });
+  }));
+
+  it('transforms account info into a picture', inject([GraphClientService, DomSanitizer], async (graph: GraphClientService, sanitizer: DomSanitizer) => {
+    const pipe = new GraphPicturePipe(graph, sanitizer);
+    const promise = lastValueFrom(pipe.transform(account));
+
+    const request = controller.expectOne(`https://graph.microsoft.com/v1.0/users/${localAccountId}/photo/$value`);
+    expect(request.request.method).toEqual('GET');
+    request.flush(new Blob(['picture'], { type: 'text/html' }));
+
+    const actual = await promise;
+    expect(actual.constructor.name).toEqual('SafeUrlImpl');
+  }));
 });
--- src/ConceptBed/concept-bed-ui/src/app/modules/backend/pipes/graph-picture.pipe.ts
+++ src/ConceptBed/concept-bed-ui/src/app/modules/backend/pipes/graph-picture.pipe.ts
@@ -1,24 +1,23 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer, SafeValue } from '@angular/platform-browser';
 import { AccountInfo } from '@azure/msal-browser';
+import { map, Observable, share } from 'rxjs';
 
 import { GraphClientService } from '../graph-client.service';
 
 @Pipe({
-  name: 'graphPicture'
+  name: 'graph'
 })
 export class GraphPicturePipe implements PipeTransform {
-  private static cache: { [id: string]: Blob | string | SafeValue } = {};
+  private static cache: { [id: string]: Observable<Blob | string | SafeValue> } = {};
 
-  constructor(private graph: GraphClientService, private sanitazer: DomSanitizer) {
+  constructor(private graph: GraphClientService, private sanitizer: DomSanitizer) {
   }
 
-  transform(account: AccountInfo | null, source?: 'my'): any {
-    const id = account?.username ?? '#unknown';
-    if (!!id && !GraphPicturePipe.cache.hasOwnProperty(id)) {
-      GraphPicturePipe.cache[id] = '';
-      const request = id === 'my' ? this.graph.getPhoto(id) : this.graph.getMyPhoto();
-      request.subscribe(blob => GraphPicturePipe.cache[id] = this.sanitazer.bypassSecurityTrustUrl(URL.createObjectURL(blob)));
+  transform(account: AccountInfo | null): Observable<Blob | string | SafeValue> {
+    const id = account?.localAccountId ?? '#unknown';
+    if (!GraphPicturePipe.cache.hasOwnProperty(id)) {
+      GraphPicturePipe.cache[id] = this.graph.getPhoto(id).pipe(map(x => this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(x))), share());
     }
     return GraphPicturePipe.cache[id];
   }
