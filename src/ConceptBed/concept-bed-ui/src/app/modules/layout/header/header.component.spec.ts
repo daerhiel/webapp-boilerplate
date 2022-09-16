156fdd34660625025866743625f7af533a9b6968
daerhiel <485120+daerhiel@users.noreply.github.com>
2022-09-16 17:14:51 +0200

    feature(Header): Add header unit tests

--- src/ConceptBed/concept-bed-ui/src/app/modules/layout/header/header.component.spec.ts
+++ src/ConceptBed/concept-bed-ui/src/app/modules/layout/header/header.component.spec.ts
@@ -1,7 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, inject, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { By } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
+import { By, Title } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MsalService } from '@azure/msal-angular';
@@ -12,6 +13,8 @@ import { clearGraphPipeCache } from '@modules/backend/pipes/graph-picture.pipe.s
 import { BackendModule, GraphPicturePipe } from '@modules/backend/backend.module';
 import { LayoutModule } from '../layout.module';
 import { HeaderComponent } from './header.component';
+
+const appTitle = 'App Title';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -31,13 +34,17 @@ describe('HeaderComponent', () => {
       declarations: [HeaderComponent],
       providers: [
         { provide: MsalService, useValue: msalServiceMock }
-      ]
+      ],
+      teardown: {
+        destroyAfterEach: false
+      }
     }).compileComponents();
     controller = TestBed.inject(HttpTestingController);
   });
 
-  beforeEach(() => {
+  beforeEach(inject([Title], (title: Title) => {
     clearGraphPipeCache();
+    title.setTitle(appTitle);
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
@@ -46,10 +53,43 @@ describe('HeaderComponent', () => {
     graphApiMock(controller, user, 'me');
     graphApiMock(controller, picture, 'users', [localAccountId, 'photo', '$value']);
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', async () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render home button with title', inject([Title], async (title: Title) => {
+    const home = fixture.debugElement.query(By.css('a[mat-button]'));
+
+    expect(home.nativeElement).not.toBeNull();
+
+    const wrapper = home.query(By.css('span.mat-button-wrapper'));
+
+    expect(wrapper.nativeElement).not.toBeNull();
+    expect(wrapper.nativeElement.innerHTML).toEqual(title.getTitle());
+  }));
+
+  fit('should render favorites button', async () => {
+    const favorites = fixture.debugElement.query(By.css('button[mat-icon-button]#favorite'));
+
+    expect(favorites.nativeElement).not.toBeNull();
+
+    const icon = favorites.query(By.css('mat-icon'));
+
+    expect(icon.nativeElement).not.toBeNull();
+    expect(icon.nativeElement.innerHTML).toEqual('favorite');
+  });
+
+  fit('should render help button', async () => {
+    const help = fixture.debugElement.query(By.css('button[mat-icon-button]#help'));
+
+    expect(help.nativeElement).not.toBeNull();
+
+    const icon = help.query(By.css('mat-icon'));
+
+    expect(icon.nativeElement).not.toBeNull();
+    expect(icon.nativeElement.innerHTML).toEqual('help');
   });
 
   it('should display current user picture', async () => {
@@ -57,6 +97,7 @@ describe('HeaderComponent', () => {
     fixture.detectChanges();
 
     const img = fixture.debugElement.query(By.css('img'));
+
     expect(img.nativeElement).not.toBeNull();
     expect(img.nativeElement.src).toMatch(/^blob:http:/i);
   });
