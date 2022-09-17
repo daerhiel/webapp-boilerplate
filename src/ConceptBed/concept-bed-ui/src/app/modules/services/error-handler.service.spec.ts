bd474aa26d55fc810c4d102aed24fb9f0aa3090c
daerhiel <485120+daerhiel@users.noreply.github.com>
2022-09-17 15:09:52 +0200

    feature(ErrorHandler): Add error handler service test

--- src/ConceptBed/concept-bed-ui/src/app/modules/services/error-handler.service.spec.ts
+++ src/ConceptBed/concept-bed-ui/src/app/modules/services/error-handler.service.spec.ts
@@ -1,16 +1,33 @@
 import { TestBed } from '@angular/core/testing';
+import { SeverityLevel } from '@microsoft/applicationinsights-web';
 
 import { ErrorHandlerService } from './error-handler.service';
+import { TelemetryService } from './telemetry.service';
 
-describe('ErrorHandlerService', () => {
+const error = new Error('Error occurred');
+const telemetryMock = jasmine.createSpyObj<TelemetryService>('TelemetryService', ['logException']);
+
+fdescribe('ErrorHandlerService', () => {
   let service: ErrorHandlerService;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({}).compileComponents();
+    await TestBed.configureTestingModule({
+      providers: [
+        { provide: TelemetryService, useValue: telemetryMock }
+      ]
+    }).compileComponents();
     service = TestBed.inject(ErrorHandlerService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should sends telemetry event on error', () => {
+    const spy = spyOn(console, 'error');
+    service.handleError(error);
+
+    expect(telemetryMock.logException).toHaveBeenCalledWith({ error, severityLevel: SeverityLevel.Error });
+    expect(spy).toHaveBeenCalled();
   });
 });
