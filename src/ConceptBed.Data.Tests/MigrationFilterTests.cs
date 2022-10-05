57fa6884c3530b7013f8fdfcbeee1b29765ecc0c
daerhiel <485120+daerhiel@users.noreply.github.com>
2022-10-05 22:05:17 +0200

    feature(Context): Add database migration tests

---
+++ src/ConceptBed.Data.Tests/MigrationFilterTests.cs
@@ -0,0 +1,45 @@
+﻿using ConceptBed.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
+
+namespace Ubiquity.Data.Tests;
+
+public class MigrationFilterTests
+{
+    protected ITestOutputHelper Output { get; }
+
+    public MigrationFilterTests(ITestOutputHelper output)
+    {
+        Output = output ?? throw new ArgumentNullException(nameof(output));
+    }
+
+    [Fact]
+    public void Configure_SqliteMemory()
+    {
+        Output.WriteLine($"Testing {new StackTrace().GetFrame(0)?.GetMethod()?.Name}.");
+
+        // Arrange
+        var services = new ServiceCollection();
+        services.AddDbContext<ConceptContext>(builder =>
+        {
+            var connection = new SqliteConnection("Filename=:memory:");
+            connection.Open();
+            builder.UseSqlite(connection);
+        });
+        var serviceProvider = services.BuildServiceProvider();
+        var migrator = new MigrationFilter<ConceptContext>(serviceProvider);
+
+        // Act
+        var actual = migrator.Configure(expected);
+
+        // Assert
+        Assert.Equal(expected, actual);
+
+        static void expected(IApplicationBuilder builder)
+        {
+        }
+    }
+}
